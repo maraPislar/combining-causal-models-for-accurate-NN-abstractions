2bc8f6ffd2d22cdac6335113c270d7bf65f3613b
Mara Pislar <pislarmara589@gmail.com>
2024-09-12 23:20:12 +0300

    fix annoying stupid bug

--- src/disentangle_causal_models.py
+++ src/disentangle_causal_models.py
@@ -12,6 +12,8 @@ from transformers import (AutoTokenizer,
                           GPT2ForSequenceClassification)
 from pyvene import IntervenableModel
 from my_pyvene.analyses.visualization import rotation_token_heatmap
+from torch.utils.data import DataLoader
+from tqdm import tqdm, trange
 
 def intervention_id(intervention):
     if "P" in intervention:
@@ -21,33 +23,62 @@ def tokenizePrompt(input, tokenizer):
     prompt = f"{input['X']}+{input['Y']}+{input['Z']}="
     return tokenizer.encode(prompt, padding=True, return_tensors='pt')
 
-def eval_one_point(intervenable, data, low_rank_dimension, min_class_value=3):
+# def eval_one_point(intervenable, data, low_rank_dimension, min_class_value=3):
+#     eval_labels = []
+#     eval_preds = []
+
+#     with torch.no_grad():
+#         for input_key in ["base_source", "source_base"]:
+#             inputs = data[input_key]
+
+#             _, counterfactual_outputs = intervenable(
+#                 {"input_ids": inputs["input_ids"]},
+#                 [{"input_ids": inputs["source_input_ids"][:, 0]}],
+#                 {"sources->base": [0, 1, 2, 3, 4, 5]},
+#                 subspaces=[
+#                     [[_ for _ in range(low_rank_dimension)]]
+#                 ]
+#             )
+
+#             eval_labels.append(inputs["labels"].type(torch.long).squeeze() - min_class_value)
+#             eval_preds.append(torch.argmax(counterfactual_outputs[0], dim=1).squeeze())
+
+#     print(eval_preds)
+#     report = classification_report(torch.tensor(eval_labels).cpu(), torch.tensor(eval_preds).cpu(), output_dict=True)
+#     return report['accuracy']
+
+def eval_one_point(intervenable, eval_data, low_rank_dimension, batch_size = 2, min_class_value=3):
+    # eval on all data
     eval_labels = []
     eval_preds = []
-
     with torch.no_grad():
-        for input_key in ["base_source", "source_base"]:
-            inputs = data[input_key]
+        epoch_iterator = tqdm(DataLoader(eval_data, batch_size), desc=f"Test")
+        for _, inputs in enumerate(epoch_iterator):
+            for k, v in inputs.items():
+                if v is not None and isinstance(v, torch.Tensor):
+                    inputs[k] = v.to("cuda")
+            inputs["input_ids"] = inputs["input_ids"].squeeze()
+            inputs["source_input_ids"] = inputs["source_input_ids"].squeeze(2)
 
             _, counterfactual_outputs = intervenable(
                 {"input_ids": inputs["input_ids"]},
                 [{"input_ids": inputs["source_input_ids"][:, 0]}],
-                {"sources->base": [0, 1, 2, 3, 4, 5]},
+                {"sources->base": [0,1,2,3,4,5]},
                 subspaces=[
-                    [[_ for _ in range(low_rank_dimension)]]
+                    [[_ for _ in range(low_rank_dimension)]] * batch_size
                 ]
             )
 
-            eval_labels.append(inputs["labels"].type(torch.long).squeeze() - min_class_value)
-            eval_preds.append(torch.argmax(counterfactual_outputs[0], dim=1).squeeze())
-
-    report = classification_report(torch.tensor(eval_labels).cpu(), torch.tensor(eval_preds).cpu(), output_dict=True)
+            eval_labels += [inputs["labels"].type(torch.long).squeeze() - min_class_value]
+            eval_preds += [torch.argmax(counterfactual_outputs[0], dim=1)]
+            
+    report = classification_report(torch.cat(eval_labels).cpu(), torch.cat(eval_preds).cpu(), output_dict=True) # get the IIA
     return report['accuracy']
 
 def main():
 
     parser = argparse.ArgumentParser(description="Process experiment parameters.")
-    parser.add_argument('--model_path', type=str, help='path to the finetuned GPT2ForSequenceClassification on the arithmetic task')
+    parser.add_argument('--model_path', type=str, default='mara589/arithmetic-gpt2', help='path to the finetuned GPT2ForSequenceClassification on the arithmetic task')
     parser.add_argument('--results_path', type=str, default='results/', help='path to the results folder')
     parser.add_argument('--causal_model_type', type=str, choices=['arithmetic', 'simple'], default='arithmetic', help='choose between arithmetic or simple')
     parser.add_argument('--low_rank_dim', type=int, default=256, help='low rank dimension for rotation intervention')
@@ -63,7 +94,7 @@ def main():
 
     args.results_path = os.path.join(args.results_path, args.causal_model_type)
 
-    save_graphs_path = os.path.join(args.results_path, 'graphs')
+    save_graphs_path = os.path.join(args.results_path, 'graphs_2')
     os.makedirs(save_graphs_path, exist_ok=True)
     
     set_seed(args.seed)
@@ -84,8 +115,8 @@ def main():
         raise ValueError(f"Invalid causal model type: {args.causal_model_type}. Can only choose between arithmetic or simple.")
 
     low_rank_dimension = args.low_rank_dim
-    # numbers = range(1, 11)
-    numbers = range(1,4)
+    numbers = range(1, 11)
+    # numbers = range(7,11)
     repeat = 3
     graph_size = len(numbers) ** repeat
     arrangements = list(product(numbers, repeat=repeat))
@@ -100,8 +131,8 @@ def main():
     # loop through the family of causal models
     for cm_id, model_info in arithmetic_family.causal_models.items():
 
-        # to only get the graphs for targetting X or X+Y+Z
-        if model_info['label'] == 'X+(Y)+Z' or model_info['label'] == 'X+Y+(Z)':
+        # to only get the graph for targetting X+Y+Z
+        if model_info['label'] == 'X+(Y)+Z' or model_info['label'] == 'X+Y+(Z)' or model_info['label'] == '(X)+Y+Z':
             continue
 
         print('loading intervenable model')
@@ -116,7 +147,11 @@ def main():
 
         for i, base in enumerate(arrangements):
             base = construct_arithmetic_input(base)
-            for j, source in enumerate(arrangements[i + 1:]):
+            for j, source in enumerate(arrangements):
+
+                if i <= j:
+                    continue
+
                 source = construct_arithmetic_input(source)
                 
                 data = model_info['causal_model'].generate_counterfactual_pairs(
@@ -127,7 +162,8 @@ def main():
                     inputFunction=lambda x: tokenized_cache[tuple(x.values())]
                 )
                 
-                iia = eval_one_point(intervenable, data, low_rank_dimension)
+                iia = eval_one_point(intervenable, list(data.values()), low_rank_dimension)
+                
                 graph_encoding[i][j] = iia
                 graph_encoding[j][i] = iia
 
