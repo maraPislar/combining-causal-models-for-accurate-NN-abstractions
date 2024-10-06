950aa02b5a18d2518d9018ff2915f504b289d9b2
Mara Pislar <pislarmara589@gmail.com>
2024-10-06 13:25:22 +0200

    update with new experiment

--- src/iia_graph_analyser.py
+++ src/iia_graph_analyser.py
@@ -1,25 +1,18 @@
 import sys, os
 sys.path.append(os.path.join('..', '..'))
 import argparse
-import pickle
-import json
 from pyvene import set_seed
 import torch
 import networkx as nx
-from networkx.readwrite import json_graph
-from itertools import product
-from utils import construct_arithmetic_input
-import itertools
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import csv
-from utils import visualize_graph
+import pickle
 
 def main():
     
     parser = argparse.ArgumentParser(description="Process experiment parameters.")
     parser.add_argument('--results_path', type=str, default='results/', help='path to the results folder where you have saved the graphs')
     parser.add_argument('--layer', type=int, default=0, help='layer corresponding to the graphs obtained that you want to analyse')
+    parser.add_argument('--faithfulness', type=lambda x: (float(x) if 0.0 <= float(x) <= 1.0 else parser.error(f"--faithfulness {x} not in range [0.0, 1.0]")))
     parser.add_argument('--low_rank_dimension', type=int, default=256, help='low_rank_dimension corresponding to the graphs obtained that you want to analyse')
     parser.add_argument('--top_k', type=int, default=100, help='get the top 100 nodes representing the best each intervenable model data')
     parser.add_argument('--seed', type=int, default=43, help='experiment seed to be able to reproduce the results')
@@ -31,115 +24,124 @@ def main():
     if not os.path.exists(args.results_path):
         raise argparse.ArgumentTypeError("Invalid results_path. Path does not exist.")
     
-    classification_path = os.path.join(args.results_path, 'classification_data_2')
-    os.makedirs(classification_path, exist_ok=True)
+    data_division_path = os.path.join(args.results_path, 'divided_data')
+    os.makedirs(data_division_path, exist_ok=True)
 
     cliques_info_path = os.path.join(args.results_path, 'cliques_info')
     os.makedirs(cliques_info_path, exist_ok=True)
 
-    # labels = ['(X)+Y+Z', 'X+(Y)+Z', 'X+Y+(Z)', '(X+Y)+Z', '(X+Z)+Y', 'X+(Y+Z)']
-    labels = ['X+(Y+Z)'] #, 'X+(Y)+Z', 'X+Y+(Z)', '(X+Y)+Z', '(X+Z)+Y', 'X+(Y+Z)']
-
-    limits = [num for num in range(10, 1001, 20)]
-    limits.append(1000)
-    # print(len(limits))
-
-    # all_accs = {}
+    all_labers = [['(X)+Y+Z'], ['X+(Y)+Z'], ['X+Y+(Z)'],
+                  ['(X+Y)+Z'], ['(X+Z)+Y'], ['X+(Y+Z)'],
+                  ['(X)+Y+Z', '(X+Y)+Z', '(X+Y+Z)'],
+                  ['X+(Y)+Z', '(X+Y)+Z', '(X+Y+Z)'],
+                  ['(X)+Y+Z', '(X+Z)+Y', '(X+Y+Z)'],
+                  ['X+Y+(Z)', '(X+Z)+Y', '(X+Y+Z)'],
+                  ['X+(Y)+Z', 'X+(Y+Z)', '(X+Y+Z)'],
+                  ['X+Y+(Z)', 'X+(Y+Z)', '(X+Y+Z)'],
+                  ['(X)+Y+Z', 'X+(Y)+Z', 'X+Y+(Z)', '(X+Y)+Z', '(X+Z)+Y', 'X+(Y+Z)', '(X+Y+Z)']]
     
-    exclude_list = set()
+    # all_faithfulness = [1.0, 0.95, 0.9, 0.8, 0.7, 0.6]
 
-    for label in labels:
-        accs = []
+    for labels in all_labers:
 
-        print(f'Loading graph for lrd {args.low_rank_dimension}, layer {args.layer}, model {label}')
-        graph_path = os.path.join(args.results_path, f'graphs/{label}_graph_{args.low_rank_dimension}_{args.layer}.pt')
-        graph = torch.load(graph_path)
-        graph.fill_diagonal_(0)
+        exclude_list = set()
 
-        print('Constructing graph..')
-        G = nx.from_numpy_array(graph.numpy())
+        # temp_labels = []
+        # for label in labels:
+        #     data_path = os.path.join(data_division_path, f'{args.faithfulness}_{label}_layer_{args.layer}.pkl')
 
-        node_degrees = dict(G.degree())
-        sorted_nodes = sorted(node_degrees, key=node_degrees.get, reverse=True)
-
-        # if label == '(X)+Y+Z':
-        #     limits = [210]
-        #     # limits = [num for num in range(10, 1001, 20)]
-        #     # limits.append(1000)
-        # elif label == '(X+Y)+Z':
-        #     # limits = [num for num in range(10, 790, 20)]
-        #     # limits.append(1000)
-        #     limits = [590]
-        # else:
-        #     # limits = [500]
-        #     limits = [num for num in range(10, 201, 20)]
-        #     limits.append(200)
-        #     # limits = [20]
-
-        # limits = [num for num in range(10, 1001, 20)]
-        # limits.append(1000)
-        print(limits)
-
-        for top_k in limits:
-
-            args.top_k = top_k
-
-            temp_exclude_list = exclude_list.copy()
-            top_k_nodes = []
-
-            iia = 0
-            temp_iia = 0
-            margin_of_error = 0
-
-            while len(top_k_nodes) < args.top_k:
-                iia_before = iia
-                for node in sorted_nodes:
-                    if node in temp_exclude_list:
-                        continue
-                    subgraph = G.subgraph([n for n in top_k_nodes + [node]])
-                    temp_iia = sum(data['weight'] for _, _, data in subgraph.edges(data=True))
-                    num_nodes = subgraph.number_of_nodes()
-                    if (num_nodes*(num_nodes - 1)/2) > 0:
-                        temp_iia = temp_iia/(num_nodes*(num_nodes - 1)/2)
-                    if temp_iia >= iia - margin_of_error:
-                        iia = temp_iia
-                        top_k_nodes.append(node)
-                    
-                    if len(top_k_nodes) >= args.top_k:
-                        break
+        #     if os.path.exists(data_path):
+        #         with open(data_path, 'rb') as f:
+        #             data = pickle.load(f)
             
-                temp_exclude_list.update(top_k_nodes)
-
-                if iia_before == iia:
-                    margin_of_error += 0.01
-
-            subgraph = G.subgraph(top_k_nodes)
-            num_nodes = subgraph.number_of_nodes()
-            iia = sum(data['weight'] for _, _, data in subgraph.edges(data=True))/(num_nodes*(num_nodes - 1)/2)
-            accs.append((top_k, iia))
-            print(top_k, iia)
-            
-            # if label == '(X)+Y+Z' or label == '(X+Y)+Z':
-            #     exclude_list.update(top_k_nodes)
-            
-            # visualize_graph(subgraph, label=label)
-            
-            # save_data_path = os.path.join(classification_path, f'{args.low_rank_dimension}')
-            # os.makedirs(save_data_path, exist_ok=True)
-            # data_path = os.path.join(save_data_path, f'data_{label}_{args.layer}.pkl')
-            
-            # with open(data_path, 'wb') as f:
-            #     pickle.dump(top_k_nodes, f)
-
-        file_name = f'{label}_iias_top_k.txt'
-        if os.path.isfile(file_name):
-            os.remove(file_name)
+        #         exclude_list.update(data)
+        #     else:
+        #         temp_labels.append(label)
         
-        with open(file_name, 'a', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['top_k', 'iia'])
-            for top_k, iia in accs:
-                writer.writerow([top_k, iia])
+        # labels = temp_labels
+
+        for label in labels:
+            accs = []
+
+            print(f'Loading graph for lrd {args.low_rank_dimension}, layer {args.layer}, model {label}')
+            graph_path = os.path.join(args.results_path, f'graphs/{label}_graph_{args.low_rank_dimension}_{args.layer}.pt')
+            graph = torch.load(graph_path)
+            graph.fill_diagonal_(0)
+
+            print('Constructing graph..')
+            G = nx.from_numpy_array(graph.numpy())
+
+            node_degrees = dict(G.degree())
+            sorted_nodes = sorted(node_degrees, key=node_degrees.get, reverse=True)
+
+            limits = [num for num in range(10, 1001 - len(exclude_list), 20)]
+
+            if label == '(X+Y+Z)':
+                limits = [1000 - len(exclude_list)]
+
+            print(limits)
+
+            for top_k in limits:
+
+                args.top_k = top_k
+
+                temp_exclude_list = exclude_list.copy()
+                top_k_nodes = []
+
+                iia = 0
+                temp_iia = 0
+                margin_of_error = 0
+
+                while len(top_k_nodes) < args.top_k:
+                    iia_before = iia
+                    for node in sorted_nodes:
+                        if node in temp_exclude_list:
+                            continue
+                        subgraph = G.subgraph([n for n in top_k_nodes + [node]])
+                        temp_iia = sum(data['weight'] for _, _, data in subgraph.edges(data=True))
+                        num_nodes = subgraph.number_of_nodes()
+                        if (num_nodes*(num_nodes - 1)/2) > 0:
+                            temp_iia = temp_iia/(num_nodes*(num_nodes - 1)/2)
+                        if temp_iia >= iia - margin_of_error:
+                            iia = temp_iia
+                            top_k_nodes.append(node)
+                        
+                        if len(top_k_nodes) >= args.top_k:
+                            break
+                
+                    temp_exclude_list.update(top_k_nodes)
+
+                    if iia_before == iia:
+                        margin_of_error += 0.01
+
+                if args.faithfulness <= iia or label == '(X+Y+Z)':
+                    temp_top_k_nodes = top_k_nodes
+
+                    subgraph = G.subgraph(top_k_nodes)
+                    num_nodes = subgraph.number_of_nodes()
+                    iia = sum(data['weight'] for _, _, data in subgraph.edges(data=True))/(num_nodes*(num_nodes - 1)/2)
+                    accs.append((top_k, iia))
+                    print(top_k, iia)
+                
+                if args.faithfulness > iia:
+                    exclude_list.update(temp_top_k_nodes)
+
+                    data_path = os.path.join(data_division_path, f'{args.faithfulness}_{label}_layer_{args.layer}.pkl')
+                
+                    with open(data_path, 'wb') as f:
+                        pickle.dump(temp_top_k_nodes, f)
+
+                    break
+
+            file_name = f'{args.faithfulness}_{label}_iias_top_k.txt'
+            if os.path.isfile(file_name):
+                os.remove(file_name)
+            
+            with open(file_name, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['top_k', 'iia'])
+                for top_k, iia in accs:
+                    writer.writerow([top_k, iia])
                 
 if __name__ =="__main__":
     main()
